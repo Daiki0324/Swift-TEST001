e3894b51684bf3bb8fdde9c63232688c68bdf597
Daiki0324 <daiki.sato.03.gmail.com>
2023-01-14 20:46:26 +0900

    Initial Commit

---
+++ TEST001/TEST001App.swift
@@ -0,0 +1,17 @@
+//
+//  TEST001App.swift
+//  TEST001
+//
+//  Created by 佐藤大起 on 2023/01/14.
+//
+
+import SwiftUI
+
+@main
+struct TEST001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
